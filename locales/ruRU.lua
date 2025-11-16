916ce81baf88314b92551861f1fd69c56571df4e
Slackluster <ruben.sluimer@gmail.com>
2025-11-16 17:46:09 +0100

    Minor locale updates

--- locales/ruRU.lua
+++ locales/ruRU.lua
@@ -33,18 +33,18 @@ L.WATER =								RELIC_SLOT_TYPE_WATER
 L.GARRISON =							GARRISON_LOCATION_TOOLTIP
 
 -- Settings
-L.SETTINGS_SUPPORT_TEXTLONG = "Разработка этого аддона требует значительного времени и усилий.\nПожалуйста, рассмотрите возможность финансовой поддержки разработчика."
-L.SETTINGS_SUPPORT_TEXT = "Поддержать"
-L.SETTINGS_SUPPORT_BUTTON = "Buy Me a Coffee" -- Brand name, if there isn't a localised version, keep it the way it is
-L.SETTINGS_SUPPORT_DESC = "Спасибо!"
-L.SETTINGS_HELP_TEXT = "Обратная связь и помощь"
-L.SETTINGS_HELP_BUTTON = "Discord" -- Brand name, if there isn't a localised version, keep it the way it is
-L.SETTINGS_HELP_DESC = "Присоединиться к серверу Discord."
-L.SETTINGS_ISSUES_TEXT = "Отслеживание ошибок"
-L.SETTINGS_ISSUES_BUTTON = "GitHub" -- Brand name, if there isn't a localised version, keep it the way it is
-L.SETTINGS_ISSUES_DESC = "Просмотреть трекер ошибок на GitHub."
-L.SETTINGS_URL_COPY = "Ctrl+C — скопировать:"
-L.SETTINGS_URL_COPIED = "Ссылка скопирована в буфер обмена"
+L.SETTINGS_SUPPORT_TEXTLONG =			"Разработка этого аддона требует значительного времени и усилий.\nПожалуйста, рассмотрите возможность финансовой поддержки разработчика."
+L.SETTINGS_SUPPORT_TEXT =				"Поддержать"
+L.SETTINGS_SUPPORT_BUTTON =				"Buy Me a Coffee" -- Brand name, if there isn't a localised version, keep it the way it is
+L.SETTINGS_SUPPORT_DESC =				"Спасибо!"
+L.SETTINGS_HELP_TEXT =					"Обратная связь и помощь"
+L.SETTINGS_HELP_BUTTON =				"Discord" -- Brand name, if there isn't a localised version, keep it the way it is
+L.SETTINGS_HELP_DESC =					"Присоединиться к серверу Discord."
+L.SETTINGS_ISSUES_TEXT =				"Отслеживание ошибок"
+L.SETTINGS_ISSUES_BUTTON =				"GitHub" -- Brand name, if there isn't a localised version, keep it the way it is
+L.SETTINGS_ISSUES_DESC =				"Просмотреть трекер ошибок на GitHub."
+L.SETTINGS_URL_COPY =					"Ctrl+C — скопировать:"
+L.SETTINGS_URL_COPIED =					"Ссылка скопирована в буфер обмена"
 
 L.SETTINGS_SPLITBAG_TITLE =				"Разделение количества мешков с реагентами"
 L.SETTINGS_SPLITBAG_TOOLTIP =			"Показывает свободные слоты ваших обычных сумок и сумки с реагентами отдельно над значком рюкзака."
