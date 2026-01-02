972b5cb9632d2544711c560c457917fc8283ae85
Slackluster <ruben.sluimer@gmail.com>
2026-01-02 12:50:30 +0100

    Add a checkmark to the link copied dialogue for visual clarity

--- Database.lua
+++ Database.lua
@@ -13,3 +13,6 @@ app.NameShort = app.Colour("STS")
 -- Used textures
 app.IconLMB = "|TInterface\\TutorialFrame\\UI-Tutorial-Frame:12:12:0:0:512:512:10:65:228:283|t"
 app.IconRMB = "|TInterface\\TutorialFrame\\UI-Tutorial-Frame:12:12:0:0:512:512:10:65:330:385|t"
+app.IconWaiting = "|TInterface\\RaidFrame\\ReadyCheck-Waiting:0|t"
+app.IconReady = "|TInterface\\RaidFrame\\ReadyCheck-Ready:0|t"
+app.IconNotReady = "|TInterface\\RaidFrame\\ReadyCheck-NotReady:0|t"
