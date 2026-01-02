67d11f64bc29b625f325614e5bcff618009dd78a
Slackluster <ruben.sluimer@gmail.com>
2026-01-02 01:59:02 +0100

    Pedantic uppercase fix

--- Settings.lua
+++ Settings.lua
@@ -79,7 +79,7 @@ function app.Settings()
 		end,
 	}
 	local function onSupportButtonClick()
-		StaticPopup_Show("SLACKERSTWEAKSUITE_URL", nil, nil, "https://buymeacoffee.com/slackluster")
+		StaticPopup_Show("SLACKERSTWEAKSUITE_URL", nil, nil, "https://buymeacoffee.com/Slackluster")
 	end
 	layout:AddInitializer(CreateSettingsButtonInitializer(L.SETTINGS_SUPPORT_TEXT, L.SETTINGS_SUPPORT_BUTTON, onSupportButtonClick, L.SETTINGS_SUPPORT_DESC, true))
 
