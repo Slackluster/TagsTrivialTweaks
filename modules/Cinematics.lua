cf3c562b397778ab148c020260db54b355dd3a7d
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-07-25 21:59:30 +0200

    Revise cinematic x maps

--- modules/Cinematics.lua
+++ modules/Cinematics.lua
@@ -12,39 +12,57 @@ app.Event:Register("ADDON_LOADED", function(addOnName, containsBindings)
 	if addOnName == appName then
 		app.Settings.cinematics = app.Settings.cinematics or {}
 
-		local function registerMovie(movieID)
-			app.Settings.cinematics[movieID] = app.Settings.cinematics[movieID] or {}
-			local date = C_DateAndTime.GetCurrentCalendarTime()
-			local mapID = C_Map.GetBestMapForUnit("player")
-			table.insert(app.Settings.cinematics[movieID], { dateTime = date.year .. "-" .. date.month .. "-" .. date.monthDay .. " " .. date.hour .. ":" .. date.minute, map = C_Map.GetMapInfo(mapID).name, mapID = mapID })
-		end
-
-		local function handleMovie(movieID, source)
-			app:Debug(source)
-			if not movieID then
-				app:Debug("No movieID found")
-			elseif app.Settings.cinematics[movieID] then
-				app:Debug("Skipped movieID", movieID)
-				C_Timer.After(2, function() registerMovie(movieID) end)
-				-- MovieFrame:Hide()
-				-- StopCinematic()
-				-- CancelScene()
-			elseif movieID then
-				app:Debug("Registering movieID", movieID)
-				C_Timer.After(2, function() registerMovie(movieID) end)
-			end
-		end
-
-		EventRegistry:RegisterCallback("CinematicFrame.CinematicStarting", function(movieID)
-			handleMovie(movieID, "CinematicFrame.CinematicStarting")
-		end)
-
-		hooksecurefunc("CinematicStarted", function(movieType, movieID, canCancel)
-			handleMovie(movieID, "CinematicStarted")
-		end)
-
-		hooksecurefunc("MovieFrame_PlayMovie", function(self, movieID) -- unconfirmed
-			handleMovie(movieID, "MovieFrame_PlayMovie")
-		end)
+		app:SkipSeenCinematics()
 	end
 end)
+
+--------------------------
+-- SKIP CEEN CINEMATICS --
+--------------------------
+
+function app:SkipSeenCinematics()
+	local function registerMovie(movieID, mapID)
+		app.Settings.cinematics[movieID] = app.Settings.cinematics[movieID] or {}
+		local date = C_DateAndTime.GetCurrentCalendarTime()
+		table.insert(app.Settings.cinematics[movieID], { dateTime = string.format("%d-%02d-%02d %02d:%02d", date.year, date.month, date.monthDay, date.hour, date.minute), map = C_Map.GetMapInfo(mapID).name, mapID = mapID })
+	end
+
+	local function handleMovie(movieID, source)
+		local mapID = C_Map.GetBestMapForUnit("player")
+		app:Debug(source)
+		if not movieID then
+			app:Debug("No movieID found")
+		elseif app.Settings.cinematics[movieID] then
+			local seenMap = false
+			for _, seen in ipairs(app.Settings.cinematics[movieID]) do
+				if seen.mapID == mapID then
+					app:Debug("Skipped movieID", movieID)
+					MovieFrame:Hide()
+					StopCinematic()
+					CancelScene()
+					seenMap = true
+					break
+				end
+			end
+			if not seenMap then
+				app:Debug("Registering movieID (new map)", movieID)
+				C_Timer.After(2, function() registerMovie(movieID, mapID) end)
+			end
+		else
+			app:Debug("Registering movieID", movieID)
+			C_Timer.After(2, function() registerMovie(movieID, mapID) end)
+		end
+	end
+
+	EventRegistry:RegisterCallback("CinematicFrame.CinematicStarting", function(movieID)
+		handleMovie(movieID, "CinematicFrame.CinematicStarting")
+	end)
+
+	hooksecurefunc("CinematicStarted", function(movieType, movieID, canCancel)
+		handleMovie(movieID, "CinematicStarted")
+	end)
+
+	hooksecurefunc("MovieFrame_PlayMovie", function(self, movieID) -- unconfirmed
+		handleMovie(movieID, "MovieFrame_PlayMovie")
+	end)
+end
