83c8b17c30e0d86d9e9aa81463fcda7e23eff269
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-07-30 04:07:20 +0200

    Different approach to cinematic skipping (thank you, DBM code)

--- modules/Cinematics.lua
+++ modules/Cinematics.lua
@@ -21,48 +21,30 @@ end)
 --------------------------
 
 function app:SkipSeenCinematics()
-	local function registerMovie(movieID, mapID)
-		app.Settings.cinematics[movieID] = app.Settings.cinematics[movieID] or {}
-		local date = C_DateAndTime.GetCurrentCalendarTime()
-		table.insert(app.Settings.cinematics[movieID], { dateTime = string.format("%d-%02d-%02d %02d:%02d", date.year, date.month, date.monthDay, date.hour, date.minute), map = C_Map.GetMapInfo(mapID).name, mapID = mapID })
-	end
-
-	local function handleMovie(movieID, source, canCancel)
-		local mapID = C_Map.GetBestMapForUnit("player")
-		app:Debug(source)
-		if not movieID then
-			app:Debug("No movieID found")
-		elseif app.Settings.cinematics[movieID] then
-			local seenMap = false
-			for _, seen in ipairs(app.Settings.cinematics[movieID]) do
-				if seen.mapID == mapID then
-					app:Debug("Skipped movieID", movieID, canCancel)
-					MovieFrame:Hide()
-					StopCinematic()
-					CancelScene()
-					seenMap = true
-					break
-				end
-			end
-			if not seenMap then
-				app:Debug("Registering movieID (new map)", movieID, canCancel)
-				C_Timer.After(2, function() registerMovie(movieID, mapID) end)
-			end
+	local function handleCinematic(key)
+		if not app.Settings.cinematics[key] then
+			app.Settings.cinematics[key] = true
 		else
-			app:Debug("Registering movieID", movieID, canCancel)
-			C_Timer.After(2, function() registerMovie(movieID, mapID) end)
+			MovieFrame:Hide()
+			StopCinematic()
+			CancelScene()
 		end
 	end
 
-	EventRegistry:RegisterCallback("CinematicFrame.CinematicStarting", function(movieID)
-		handleMovie(movieID, "CinematicFrame.CinematicStarting")
+	EventRegistry:RegisterCallback("CinematicFrame.CinematicStarting", function()
+		local map = C_Map.GetBestMapForUnit("player")
+		if not map then return end
+		local zone = GetZoneText() or ""
+		local subzone = GetSubZoneText() or ""
+
+		local key = "cinematic:" .. map .. ":" .. zone .. ":" .. subzone
+		handleCinematic(key)
 	end)
 
 	hooksecurefunc("CinematicStarted", function(movieType, movieID, canCancel)
-		handleMovie(movieID, "CinematicStarted", canCancel)
-	end)
+		if not movieID then return end
 
-	hooksecurefunc("MovieFrame_PlayMovie", function(self, movieID) -- unconfirmed
-		handleMovie(movieID, "MovieFrame_PlayMovie")
+		local key = "movie:" .. movieID
+		handleCinematic(key)
 	end)
 end
